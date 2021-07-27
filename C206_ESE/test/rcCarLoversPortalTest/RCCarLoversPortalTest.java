b5effb1131580b1c0f7addeb04f08f987ba54d5a
ShaunTheSleepingSheep <>
2021-07-27 09:41:21 +0800

    Created RCCarLoversTest.java (JUnit test class).

---
+++ C206_ESE/test/rcCarLoversPortalTest/RCCarLoversPortalTest.java
@@ -0,0 +1,14 @@
+package rcCarLoversPortalTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RCCarLoversPortalTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
