42aab337d47dbf38620a611021370e124060fd5f
ShaunTheSleepingSheep <>
2021-07-27 09:18:52 +0800

    Created RCCarLoversPortal.java

---
+++ C206_ESE/src/rcCarLoversPortal/RCCarLoversPortal.java
@@ -0,0 +1,10 @@
+package rcCarLoversPortal;
+
+public class RCCarLoversPortal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
