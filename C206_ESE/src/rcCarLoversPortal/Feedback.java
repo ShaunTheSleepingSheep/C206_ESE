d7291d479b1291031fd726f188e9bb0291fdf00b
ShaunTheSleepingSheep <>
2021-08-15 11:42:38 +0800

    Added 'status' attribute to Feedback class
    - Created private string called status.
    - Default value for status is 'PENDING'.
    - Created getter method for status.
    - Created setter method for status.

--- C206_ESE/src/rcCarLoversPortal/Feedback.java
+++ C206_ESE/src/rcCarLoversPortal/Feedback.java
@@ -6,11 +6,13 @@ public class Feedback extends Buyer {
 
 	private Date dateCreated;
 	private String description;
+	private String status;
 
 	public Feedback(String name, int phoneNo, String description) {
 		super(name, phoneNo);
 		dateCreated = new Date();
 		this.description = description;
+		status = "PENDING";
 	}
 
 	public Date getDateCreated() {
@@ -21,7 +23,15 @@ public class Feedback extends Buyer {
 		return description;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
 	}
 }
